428f733596681f223ad59d1d9bb8c5950d5944ee
Rashed kewan <kewan.rashed@gmail.com>
2022-07-15 03:28:50 +0300

    Adding multiple cars and improve car's features

--- Models/Car.py
+++ Models/Car.py
@@ -1,7 +1,11 @@
 import pygame
 
+
 class Car:
-    def __init__(self, x_position, y_position, image):
+    def __init__(self, x_position, y_position, image, speed=5, intersection_detected=False, can_move=True):
         self.x_position = x_position
         self.y_position = y_position
         self.image = image
+        self.speed = speed
+        self.can_move = can_move
+        self.intersection_detected = intersection_detected
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from sys import exit
+import numpy as np
 import pygame
+import time
 import os
 from Models.Button import Button
 from Models.Car import Car
@@ -24,8 +26,8 @@ pygame.display.set_caption(WINDOW_NAME)
 ############################################################
 
 # VARS
-handle_running = True
-intersection_detected = False
+paused = True
+road_map = np.zeros([1000, 1000], dtype=int)
 
 # FINALS
 ASSETS_PACKAGE = 'Assets'
@@ -54,92 +56,130 @@ stop_button = Button(1100, 0, STOP, WIN)
 ############################################################
 
 
-def draw_window(car):
-    global handle_running
+def draw_window(cars):
+    global paused
     global stop_pressed, start_pressed
     start_pressed, stop_pressed = False, False
 
     WIN.fill(GRAY)
     WIN.blit(road.image, (road.x_position, road.y_position))
-    WIN.blit(car.image, (car.x_position, car.y_position))
 
-    if handle_running:
+    for car in cars:
+        WIN.blit(car.image, (car.x_position, car.y_position))
+
+    if paused:
         start_pressed = start_button.draw()
         if start_pressed:
-            handle_running = False
+            paused = False
     else:
         stop_pressed = stop_button.draw()
         if stop_pressed:
-            handle_running = True
+            paused = True
 
     pos = pygame.mouse.get_pos()
-    print(pos)
+    # print(pos)
     pygame.display.update()
 
+# counter for trafic signal
+def countdown(car, t):
+    car.can_move = False
+    while t:
+        mins, secs = divmod(t, 60)
+        timer = '{:02d}:{:02d}'.format(mins, secs)
+        print(timer, end="\r")
+        time.sleep(1)
+        t -= 1
+    car.can_move = True
 
-def move_yellow_car(car):
-    global intersection_detected
-    global handle_running
 
-    if handle_running == False:
+def move_car(car):
+    global paused
+    global road_map
+
+    #road_map[car.y_position-road.x_position][car.y_position-road.y_position] = 0
+
+    if not paused and car.can_move:
         if car.y_position < 400 and car.x_position == 120:
-            car.y_position = car.y_position+10
+            car.y_position = car.y_position+car.speed
 
         elif car.y_position >= 400 and car.y_position < 490 and car.x_position < 240:
             car.x_position = car.x_position+5
             car.y_position = car.y_position+5
 
-        elif car.x_position < 920 and car.y_position == 490 and not intersection_detected:
-            car.x_position = car.x_position + 10
+        elif car.x_position < 920 and car.y_position == 490 and not car.intersection_detected:
+            car.x_position = car.x_position + car.speed
 
         elif car.y_position < 570 and car.x_position < 1000 and car.x_position >= 920:
             car.x_position = car.x_position + 5
             car.y_position = car.y_position + 5
-            intersection_detected = True
+            car.intersection_detected = True
 
         elif car.y_position <= 800 and car.x_position >= 1000:
-            car.y_position = car.y_position+10
+            car.y_position = car.y_position+car.speed
 
         elif car.y_position >= 800 and car.y_position <= 900 and car.x_position > 900 and car.x_position <= 1000:
             car.y_position = car.y_position+5
             car.x_position = car.x_position-5
 
         elif car.y_position > 900 and car.x_position > 700:
-            car.x_position = car.x_position-10
+            car.x_position = car.x_position-car.speed
 
         elif car.y_position > 790 and car.x_position > 590:
             car.x_position = car.x_position - 5
             car.y_position = car.y_position - 5
 
-        elif car.y_position > 90 and intersection_detected:
-            car.y_position = car.y_position - 10
+        elif car.y_position > 90 and car.intersection_detected:
+            car.y_position = car.y_position - car.speed
 
         elif car.x_position > 520 and car.y_position > 20 and car.y_position <= 100:
             car.x_position = car.x_position - 5
             car.y_position = car.y_position - 5
-            intersection_detected = False
+            car.intersection_detected = False
 
         elif car.y_position <= 35 and car.x_position < 520 and car.x_position > 220:
-            car.x_position = car.x_position - 10
+            car.x_position = car.x_position - car.speed
         else:
             car.y_position = car.y_position+5
             car.x_position = car.x_position-5
+        ########### overriding cars problem
+        # if road_map[car.y_position-road.x_position + 80][car.y_position-road.y_position] == 1  and car.x_position+80 < 920:
+        #        car.speed = 5
+        #road_map[car.y_position -road.x_position][car.y_position-road.y_position] = 1
+        #print('( ', car.x_position, ' , ', car.y_position, ' )')
+
+
+    ############ detect car with red signal light
+    #if (445 == car.x_position) and (490 == car.y_position):
+    #    countdown(car, 3)      
     return car
 
+############################################################
+########################  Main  ############################
+############################################################
 
 def main():
     clock = pygame.time.Clock()
     run = True
-    yellow_car = Car(120, 100, Image(40, 40, 'Assets',
+
+    # create cars
+    yellow_car = Car(120, 100, Image(40, 40, ASSETS_PACKAGE,
                      'yellow car.png').create_image())
+    green_car = Car(830, 490, Image(40, 40, ASSETS_PACKAGE,
+                    'green car.png').create_image())
+    red_car = Car(590, 670, Image(40, 40, ASSETS_PACKAGE,
+                  'red car.png').create_image(), 15, True)
+    cars = [yellow_car, green_car, red_car]
+
 
     while run:
         clock.tick(FPS)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-        draw_window(yellow_car)
-        yellow_car = move_yellow_car(yellow_car)
+        draw_window(cars)
+
+        for car in cars:
+            car = move_car(car)
     pygame.quit()
 
 
