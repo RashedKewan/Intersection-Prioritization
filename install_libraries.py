601572edd9e007fc2b0753dd7590af6b4bd82705
Rashed kewan <kewan.rashed@gmail.com>
2023-01-07 06:49:31 +0200

    update readme file & libraries versions

--- install_libraries.py
+++ install_libraries.py
@@ -2,13 +2,14 @@ import subprocess
 
 
 libraries = [
-    'pandas',
-    'numpy',
+    'pygame==2.1.2',
+    'pandas==1.1.5',
+    'numpy==1.21.6',
     'xlrd==1.2.0',
     'openpyxl==3.0.10',
-    'PyPDF2',
-    'reportlab',
-    'matplotlib'
+    'PyPDF2==3.0.1',
+    'reportlab==3.6.12',
+    'matplotlib==3.5.3'
 ]
 
 for library in libraries:
